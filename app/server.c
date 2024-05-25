5ab565d7ef147b9dbcc8327fc62464406b634aec
VArtz <farrelnikoson@gmail.com>
2024-05-25 19:44:46 +0700

    return http response

--- app/server.c
+++ app/server.c
@@ -50,7 +50,7 @@ int main() {
 	int client_socket_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
 
-    char response[] = "HTTP 1.1 200 OK\r\n\r\n";
+    char response[] = "HTTP/1.1 200 OK\r\n\r\n";
     send(client_socket_fd, response, sizeof(response), 0);
 
 	close(server_fd);
