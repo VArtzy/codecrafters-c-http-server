3a85cd7714cb0c02ad7986357bb7cbab7ddea313
VArtz <farrelnikoson@gmail.com>
2024-05-25 21:32:56 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -60,7 +60,7 @@ int main() {
         const char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
         char *response;
         asprintf(&response, format, contentLength, content);
-        send(conn, response, sizeof(response), 0);
+        send(conn, *response, sizeof(response), 0);
     } else if (strcmp(path, "/") == 0) {
         char response[] = "HTTP/1.1 200 OK\r\n\r\n";
         send(conn, response, sizeof(response), 0);
