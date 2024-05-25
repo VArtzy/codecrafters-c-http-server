dcd1ca83e5221b17b6f5904032826664bbf6a557
VArtz <farrelnikoson@gmail.com>
2024-05-25 21:20:00 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -58,9 +58,9 @@ int main() {
         size_t contentLength = strlen(path) - 6;
         char *content = path + 6;
         const char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
-        char response[] = malloc(512);
-        send(conn, response, sizeof(response), 0);
+        char *response = malloc(512);
         sprintf(response, format, contentLength, content);
+        send(conn, response, sizeof(response), 0);
     } else if (strcmp(path, "/") == 0) {
         char response[] = "HTTP/1.1 200 OK\r\n\r\n";
         send(conn, response, sizeof(response), 0);
