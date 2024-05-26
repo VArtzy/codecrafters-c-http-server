bf51a3a0f980319701bbb0f4954552c45cae16da
VArtz <farrelnikoson@gmail.com>
2024-05-26 16:46:19 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -61,12 +61,13 @@ void http_handler(int conn) {
         size_t contentLength = strlen(path) - 6;
         char *content = path + 6;
         char response[1024];
+        char *format;
         if (strcmp(contentEncoding, "Accept-Encoding: gzip") == 0) {   
             printf("nfrfr");
-            const char *format = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
+            *format = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
         } else {
             printf("Frfr");
-            const char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
+            *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
         }
         sprintf(response, format, contentLength, content);
         send(conn, response, sizeof(response), 0);
