eb280657517c288afdb3f7b6dfac478c37d7f836
VArtz <farrelnikoson@gmail.com>
2024-05-26 16:56:58 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -61,6 +61,11 @@ void http_handler(int conn) {
         size_t contentLength = strlen(path) - 6;
         char *content = path + 6;
         char response[1024];
+        if (strcmp(contentEncoding, "Accept-Encoding: gzip") == 0) {
+            printf("sigma");
+        } else {
+            printf("beta");
+        }
         const char *format = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
         sprintf(response, format, contentLength, content);
         send(conn, response, sizeof(response), 0);
