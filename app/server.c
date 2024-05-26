8d257751a49d85574b2df5b95bb29987110d4c38
VArtz <farrelnikoson@gmail.com>
2024-05-26 17:35:10 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -62,10 +62,9 @@ void http_handler(int conn) {
         char *content = path + 6;
         char response[1024];
         printf(contentEncoding);
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", contentLength, content);
         if (strcmp(contentEncoding, "Accept-Encoding: gzip") == 0) {
             sprintf(response, "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", contentLength, content);
-        } else if (strcmp(contentEncoding, "Accept-Encoding: invalid-encoding") == 0) {
-            sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", contentLength, content);
         }
         send(conn, response, sizeof(response), 0);
     } else if (strcmp(path, "/") == 0) {
