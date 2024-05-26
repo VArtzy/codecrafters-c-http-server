c4ab7fbf3ac48484f98a74dd902b42347fbb1cf0
VArtz <farrelnikoson@gmail.com>
2024-05-26 17:54:27 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -61,6 +61,7 @@ void http_handler(int conn) {
         size_t contentLength = strlen(path) - 6;
         char *content = path + 6;
         char response[1024];
+        printf(contentEncoding);
         if (contentEncoding == "Accept-Encoding: gzip") {
             sprintf(response, "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", contentLength, content);
         } else {
