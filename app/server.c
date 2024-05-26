5d9fca2e1990203fb20dd41a271291d446067c25
VArtz <farrelnikoson@gmail.com>
2024-05-26 20:09:07 +0700

    response body

--- app/server.c
+++ app/server.c
@@ -23,7 +23,7 @@ int compressToGzip(const char *input, int inputSize, char *output, int outputSiz
   zs.next_out = (Bytef *)output;
   deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 | 16, 8,
                Z_DEFAULT_STRATEGY);
-  deflate(&zs, Z_FINISH);
+  // deflate(&zs, Z_FINISH);
   deflateEnd(&zs);
   return zs.total_out;
 }
